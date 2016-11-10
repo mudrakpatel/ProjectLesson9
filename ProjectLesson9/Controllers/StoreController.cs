613801f8917ec3500dd3f95e2c772d536c9c22f6
mudrakpatel <mudrakpatel96@gmail.com>
2016-11-09 22:30:54 -0600

    Attempt to cope up with my code to match Sir's code

---
+++ ProjectLesson9/Controllers/StoreController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectLesson9.Models;
+
+namespace ProjectLesson9.Controllers
+{
+    public class StoreController : Controller
+    {
+        private MVCMusicStoreContext db = new MVCMusicStoreContext();
+
+        // GET: Store
+        public ActionResult Index()
+        {
+            List<Genre> genres = new List<Genre>
+            {
+                new Genre("Disco"),
+                new Genre("Jazz"),
+                new Genre("Rock"),
+                new Genre("Electronic")
+            };
+
+            return View(genres);
+        }
+
+        // GET: Store/Browse?genre=Disco
+        public ActionResult Browse(string genre)
+        {
+            if (genre == String.Empty)
+            {
+                genre = "Disco";
+            }
+
+            Genre genreModel = new Genre(genre);
+
+            return View(genreModel);
+        }
+
+        // GET: Store/Details/5
+        public ActionResult Details(int? id = 1)
+        {
+            Album album = new Album("Album " + id);
+
+            return View(album);
+        }
+    }
+}
